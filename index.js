2f595f86fa3d261b3304f9ccd0e6f44167e367d9
Soyayeb Hasan Shafin <sa.fine@outlook.com>
2023-05-09 15:04:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4f4qc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4f4qc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
